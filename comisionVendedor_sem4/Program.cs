e3fc9d16688402183add3a6e432ab368d7f1fa62
sijue666 <sijue23@gmail.com>
2024-09-22 08:32:23 -0600

    estructura básica de ej. 3 y 4

---
+++ comisionVendedor_sem4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComisionVendedorSem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ volantesVendedor_sem4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace volantesVendedor_sem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
